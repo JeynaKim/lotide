40d0eecc3d55234d549a24b4874b783e5000af29
Jeyna Kim <jeyna.kimm@gmail.com>
2022-05-05 01:03:59 +0000

    countOnly final

---
+++ countLetters.js
@@ -0,0 +1,5 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${ actual } === ${ expected }`);
+  } else console.log(`Assertion failed: ${ actual }!== ${ expected }`);
+};
