3462ba54b314c709111691533e6f06abf85606c9
Jeyna Kim <jeyna.kimm@gmail.com>
2022-05-05 04:54:34 +0000

    eqObjects final

--- eqObjects.js
+++ eqObjects.js
@@ -4,39 +4,42 @@ const assertEqual = function(actual, expected) {
   } else console.log(`Assertion failed: ${ actual }!== ${ expected }`);
 };
 
-
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
-const eqObjects = function (object1, object2) {
-  if(Object.keys(obj1).length !== Object )
-
-    for (let key in obj1) {
-  
-      if(Array.)
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
 }
-    
-    
-    
-};
+return true;
+}
 
+const eqObjects = function (obj1, obj2) {
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+    return false;
+  }
 
-
-// 길이자체가 다르면 비교가필요없으니까 
-
-
-
+  for (let key in obj1) {
+    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+      if (!eqArrays(obj1[key], obj2[key])) {
+        return false;
+      }
+    } else if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-eqObjects(ab, ba); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-eqObjects(ab, abc); // => false
-
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-eqObjects(cd, dc); // => true
+assertEqual(eqObjects(cd, dc),true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-eqObjects(cd, cd2); // => false
+assertEqual(eqObjects(cd, cd2),false)
