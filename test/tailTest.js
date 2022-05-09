cae6835e6f35677856fbb5cbcc6319a0013c6c7e
Jeyna Kim <jeyna.kimm@gmail.com>
2022-05-09 23:38:16 +0000

    M&C tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,15 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+
+describe("#tail", () => {
+
+  it("should return ['Lighthouse', 'Labs'] for ['YO YO', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['YO YO', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs'])
+  });
+
+  it("should return [2, 3, 4, 5, 6] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5, 6]), [2, 3, 4, 5, 6])
+  });
+
+});
