f55983a045b4aa40464ee77763bd29e7e8125cf6
Jeyna Kim <jeyna.kimm@gmail.com>
2022-05-09 05:26:33 +0000

    takeUntil final

--- takeUntil.js
+++ takeUntil.js
@@ -10,10 +10,10 @@ function eqArrays(arr1, arr2) {
 const assertArraysEqual = function (arr1, arr2) {
   let result = eqArrays(arr1,arr2);
   if (result === true) {
-    console.log(`Yes! ${arr1} and ${arr2} are equal!`);
+    console.log(`✅✅✅ Yes! ${arr1} and ${arr2} are equal!`);
     return result;
   } else {
-    console.log(`Unfortunately, ${arr1} and ${arr2} are not equal!`);
+    console.log(`🛑🛑🛑 Unfortunately, ${arr1} and ${arr2} are not equal!`);
     return result;
   }
 };
