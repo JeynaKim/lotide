3a38cb742ae3eb69230e5678883d497470909acb
Jeyna Kim <jeyna.kimm@gmail.com>
2022-05-09 19:26:20 +0000

    Refactor tail

--- tail.js
+++ tail.js
@@ -1,16 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${ actual } === ${ expected }`);
-  } else console.log(`Assertion failed: ${ actual }!== ${ expected }`);
+const tail = function (arr) {
+  const slicedArray = arr.slice(-1);
+  return slicedArray[0]
 };
 
-const tail = function(arr) {
-  return arr.slice(1);
-};
 
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result, ["Lighthouse", "Labs"]);
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+module.exports = tail;
