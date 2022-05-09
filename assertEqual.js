43c38229e6dd172165a5de2686957b80160cdfd6
Jeyna Kim <jeyna.kimm@gmail.com>
2022-05-09 18:16:43 +0000

    Test Code for assertEqualTest

--- assertEqual.js
+++ assertEqual.js
@@ -4,6 +4,4 @@ const assertEqual = function(actual, expected) {
   } else console.log(`Assertion failed: ${ actual }!== ${ expected }`);
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
---
+++ test/headTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+function head(arr) {
+  return arr[0]
+}
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
