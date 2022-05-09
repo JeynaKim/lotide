c300fdbe41e40e68df68a0e5e271feb0c64e47aa
Jeyna Kim <jeyna.kimm@gmail.com>
2022-05-09 23:20:16 +0000

    M&C middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,5 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
 let test1 = [1]; // => [0]
 let test2 = [1,2,3]; // => [2]
@@ -7,14 +7,26 @@ let test3 = [1,2,3,4,5]; // => [3]
 let test4 = [1,2,3,4]; // => [2,3]
 let test5 = [1,2,3,4,5,6]; // => [3,4]
 
-console.log(middle(test1));
-console.log(middle(test2));
-console.log(middle(test3));
-console.log(middle(test4));
-console.log(middle(test5));
+describe("#middle", () => {
 
-assertArraysEqual(middle(test1),test1);
-assertArraysEqual(middle(test2),test2);
-assertArraysEqual(middle(test3),test3);
-assertArraysEqual(middle(test4),test4);
-assertArraysEqual(middle(test5), test5);
+  it("should returns [0] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("should returns [2] for [1,2,3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("should returns [3] for [1,2,3,4,5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("should returns [2,3] for [1,2,3,4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("should returns [3,4] for [1,2,3,4,5,6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+});
