b7cfd1084a909b7b1dac2257732a7cb5ffc4cfab
Jeyna Kim <jeyna.kimm@gmail.com>
2022-05-10 02:27:12 +0000

    index fixed

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const middle = require('./middle');
 const assertArraysEqual = require("./assertArraysEqual");
 const assertEqual = require("./assertEqual");
 const assertObjectsEqual = require("./assertObjectsEqual");
-const countLetters = require("./countLetter");
+const countLetters = require("./countLetters");
 const countOnly = require("./countOnly");
 const eqArrays = require("./eqArrays");
 const eqObjects = require("./eqObjects");
 const findKey = require("./findKey");
-const findKeyValue = require("./findKeyValue");
+const findKeyByValue = require("./findKeyByValue");
 const flatten = require("./flatten");
 const letterPositions = require("./letterPositions");
 const map = require("./map");
@@ -18,5 +18,5 @@ const takeUntil = require("./takeUntil");
 const without = require("./without");
 
 
-module.exports = { head, tail, middle, assertArraysEqual, assertEqual, assertObjectsEqual, countLetters, countOnly, eqArrays, eqObjects, findKey, findKeyValue, flatten, letterPositions, map, takeUntil, without };
+module.exports = { head, tail, middle, assertArraysEqual, assertEqual, assertObjectsEqual, countLetters, countOnly, eqArrays, eqObjects, findKey, findKeyByValue, flatten, letterPositions, map, takeUntil, without };
 
