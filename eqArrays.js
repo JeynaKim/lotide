b9f2597297b948a6a740462eafe717b6144a32f8
Jeyna Kim <jeyna.kimm@gmail.com>
2022-05-10 04:32:07 +0000

    eqArrays final

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${ actual } === ${ expected }`);
-  } else console.log(`Assertion failed: ${ actual }!== ${ expected }`);
-};
-
 function eqArrays(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
@@ -13,12 +7,5 @@ function eqArrays(arr1, arr2) {
   return true;
 }
 
-
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
 
